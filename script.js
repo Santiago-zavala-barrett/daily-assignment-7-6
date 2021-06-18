f19c6a8ba414fdd9507cb96d52b94861ab8f2aa8
Santiago Barrett <84517048+Santiago-zavala-barrett@users.noreply.github.com>
2021-06-18 17:41:35 +0000

    fixing more errors

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ while (guess !== number) {
   guess = ('Guess a number between 1 and 10')
 }
 
-  if (guess < number) {
-    guess = prompt ('Too small, guess another number between 1 and 10')
-    }
-  else if (guess > number) {
-    guess = prompt ('Too large, guess another number between 1 and 10')
-    }
+if (guess < number) {
+  guess = prompt('Too small, guess another number between 1 and 10') 
+}
+else if (guess > number) {
+  guess = prompt('Too large, guess another number between 1 and 10')
+}
 console.log('You won! Game has ended!')
